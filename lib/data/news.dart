88851e519b9b0b546b61da1757e7cb7695b4c6cd
mikios34 <haylemikaeltefera@gmail.com>
2024-06-03 17:51:32 +0300

    feat: search news data provider done

--- lib/data/news.dart
+++ lib/data/news.dart
@@ -8,12 +8,25 @@ class NewsDataProvider {
   NewsDataProvider({
     required this.httpClient,
   });
-  
-  static const String _apiKey = '92207214a1ac4040a6978e52a0973df1';
-  static const String _baseUrl = 'https://newsapi.org/v2/top-headlines?sources=techcrunch';
 
-   Future<List<Article>> fetchNewsArticles() async {
+  static const String _apiKey = '92207214a1ac4040a6978e52a0973df1';
+  static const String _baseUrl =
+      'https://newsapi.org/v2/top-headlines?country=us';
+
+  Future<List<Article>> fetchNewsArticles() async {
     final response = await http.get(Uri.parse('$_baseUrl&apiKey=$_apiKey'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body)['articles'] as List<dynamic>;
+      return data.map((article) => Article.fromJson(article)).toList();
+    } else {
+      throw Exception('Failed to fetch news articles');
+    }
+  }
+
+  Future<List<Article>> searchNewsArticles({required String queryText}) async {
+    final response =
+        await http.get(Uri.parse('$_baseUrl&apiKey=$_apiKey&q=$queryText'));
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body)['articles'] as List<dynamic>;
