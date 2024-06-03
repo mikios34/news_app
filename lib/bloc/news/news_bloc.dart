feda147cb6e303c9718e62cde4d12dab594fc302
mikios34 <haylemikaeltefera@gmail.com>
2024-06-03 17:52:05 +0300

    feat: search news bloc added

--- lib/bloc/news/news_bloc.dart
+++ lib/bloc/news/news_bloc.dart
@@ -15,6 +15,7 @@ class NewsBloc extends Bloc<NewsEvent, NewsState> {
   NewsBloc() : super(NewsLoading()) {
     newsRepository = getIt.get<NewsRepository>();
     on<NewsLoad>(newsLoadHandler);
+    on<NewsSearch>(newsSearchHandler);
   }
 
   FutureOr<void> newsLoadHandler(
@@ -22,6 +23,18 @@ class NewsBloc extends Bloc<NewsEvent, NewsState> {
     emit(NewsLoading());
     try {
       final articles = await newsRepository.getNewsArticles();
+      emit(NewsLoadSuccess(articles: articles));
+    } catch (_) {
+      emit(NewsOperationFailure());
+    }
+  }
+
+  FutureOr<void> newsSearchHandler(
+      NewsSearch event, Emitter<NewsState> emit) async {
+    emit(NewsLoading());
+    try {
+      final articles =
+          await newsRepository.searchNewsArticles(queryText: event.queryText);
       emit(NewsLoadSuccess(articles: articles));
     } catch (_) {
       emit(NewsOperationFailure());
--- lib/bloc/news/news_event.dart
+++ lib/bloc/news/news_event.dart
@@ -7,3 +7,10 @@ class NewsLoad extends NewsEvent {
   @override
   List<Object?> get props => [];
 }
+
+class NewsSearch extends NewsEvent {
+  final String queryText;
+  NewsSearch({required this.queryText});
+  @override
+  List<Object?> get props => [queryText];
+}
