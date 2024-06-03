b0f9eb83f071c2e926b109fd3fe74a21947b5269
mikios34 <haylemikaeltefera@gmail.com>
2024-06-03 16:14:26 +0300

    UI: article card widget added

---
+++ lib/screen/widgets/article_card.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/model/article.dart';
+import 'package:news_app/screen/article_detail_page.dart';
+
+class ArticleCard extends StatelessWidget {
+  final Article article;
+  const ArticleCard({required this.article, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) =>
+                  ArticleDetailPage(articleUrl: article.sourceUrl),
+            ));
+      },
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: FadeInImage.assetNetwork(
+                placeholder:
+                    'assets/images/placeholder.png', // Placeholder image
+                image: article.imageUrl,
+                width: 100.0,
+                height: 100.0,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(width: 16.0),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    article.title,
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 4.0),
+                  Text(
+                    article.source,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
