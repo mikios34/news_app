d4a85ca0115d3b2c720db21a1c02aa717320de32
mikios34 <haylemikaeltefera@gmail.com>
2024-06-04 18:49:36 +0300

    test: widget test code added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,134 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// Mock classes for the blocs
+import 'dart:math';
 
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:news_app/bloc/news/news_bloc.dart';
+import 'package:news_app/cubit/theme_cubit.dart';
+import 'package:news_app/screen/home_page.dart';
+import 'package:news_app/screen/widgets/article_card.dart';
+import 'package:news_app/theme.dart';
 
-import 'package:news_app/main.dart';
+import 'news_bloc_test.dart';
+
+class MockNewsBloc extends MockBloc<NewsEvent, NewsState> implements NewsBloc {}
+
+class MockThemeCubit extends MockCubit<ThemeState> implements ThemeCubit {}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('HomePage Widget Test', () {
+    late MockNewsBloc mockNewsBloc;
+    late MockThemeCubit mockThemeCubit;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    setUp(() {
+      mockNewsBloc = MockNewsBloc();
+      mockThemeCubit = MockThemeCubit();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      // Ensure initial state for NewsBloc
+      // when(mockNewsBloc.state).thenReturn(NewsLoading());
+      whenListen<NewsState>(
+        mockNewsBloc,
+        Stream<NewsState>.fromIterable([NewsLoading()]),
+        initialState: NewsLoading(),
+      );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Ensure initial state for ThemeCubit
+      // when(mockThemeCubit.state).thenReturn(ThemeState(ThemeMode.system));
+      whenListen<ThemeState>(
+        mockThemeCubit,
+        Stream<ThemeState>.fromIterable([ThemeState(ThemeMode.system)]),
+        initialState: ThemeState(ThemeMode.system),
+      );
+    });
+
+    Widget createWidgetUnderTest() {
+      return MultiBlocProvider(
+        providers: [
+          BlocProvider<NewsBloc>(
+            create: (context) => mockNewsBloc,
+          ),
+          // BlocProvider<ThemeCubit>(
+          //   create: (context) => mockThemeCubit,
+          // ),
+        ],
+        child: MaterialApp(
+          theme: lightTheme,
+          darkTheme: darkTheme,
+          home: HomePage(),
+        ),
+      );
+    }
+
+    testWidgets('renders HomePage with initial state',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(createWidgetUnderTest());
+
+      expect(find.byType(AppBar), findsOneWidget);
+      expect(find.text("News Demo "), findsOneWidget);
+      expect(find.byType(TextFormField), findsOneWidget);
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    });
+
+    testWidgets(
+        'displays list of articles when NewsLoadSuccess state is emitted',
+        (WidgetTester tester) async {
+      whenListen<NewsState>(
+        mockNewsBloc,
+        Stream<NewsState>.fromIterable([NewsLoadSuccess(articles: articles)]),
+        initialState: NewsLoadSuccess(articles: articles),
+      );
+      await tester.pumpWidget(createWidgetUnderTest());
+      await tester.pump();
+      expect(find.byType(ListView), findsOneWidget);
+      expect(find.byType(ArticleCard), findsNWidgets(1));
+      expect(find.text('Chris Gale'), findsOneWidget);
+    });
+
+    testWidgets(
+        'displays error message when NewsOperationFailure state is emitted',
+        (WidgetTester tester) async {
+      whenListen<NewsState>(
+        mockNewsBloc,
+        Stream<NewsState>.fromIterable([NewsOperationFailure()]),
+        initialState: NewsOperationFailure(),
+      );
+
+      await tester.pumpWidget(createWidgetUnderTest());
+      await tester.pump();
+
+      expect(find.text('News Load Operation Failure.'), findsOneWidget);
+    });
+
+    testWidgets('toggles theme mode when selected from PopupMenuButton',
+        (WidgetTester tester) async {
+      // whenListen(
+      //   mockNewsBloc,
+      //   Stream<NewsState>.fromIterable([NewsLoading()]),
+      //   initialState: NewsLoading(),
+      // );
+      await tester.pumpWidget(createWidgetUnderTest());
+
+      // await tester.pumpAndSettle();
+      expect(find.byIcon(Icons.more_vert), findsOneWidget);
+      await tester.tap(find.byIcon(Icons.more_vert));
+      // expect(find.text("Light"), findsOneWidget);
+
+      // await tester.tap(find.text('Dark'));
+      // verify(mockThemeCubit.toggleTheme(ThemeMode.dark)).called(1);
+    });
+
+    testWidgets('searches news when search icon is pressed',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(createWidgetUnderTest());
+
+      await tester.enterText(find.byType(TextFormField), 'flutter');
+      await tester.tap(find.byIcon(Icons.search));
+      await tester.pump();
+
+      // verify(mockNewsBloc.add(NewsSearch(queryText: 'flutter'))).called(1);
+    });
   });
 }
